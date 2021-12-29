43795d4186edc22549acc9fc9f094edad6c4a85e
unic0rn9k <unic0rn9k@protonmail.com>
2021-12-29 05:51:11 +0100

    Renamed action and moved badge

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
+//! [![Workflow Status](https://github.com/unic0rn9k/slas/actions/workflows/rust.yml/badge.svg)](https://github.com/unic0rn9k/slas/actions/workflows/rust.yml)
 //! Static Linear Algebra System. A object oriented expansion of blas/blis, that allow for statically allocated cow (Copy On Write) vectors, matricies and tensors.
-//!
-//! ## Status
-//! [![Rust](https://github.com/unic0rn9k/slas/actions/workflows/rust.yml/badge.svg)](https://github.com/unic0rn9k/slas/actions/workflows/rust.yml)
 //!
 //! ## Example
 //! ```rust
