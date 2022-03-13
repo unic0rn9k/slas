63567b498f197af9998ee936d6ea0c3e6f66d9ce
unic0rn9k <unic0rn9k@protonmail.com>
2022-03-13 20:36:20 +0100

    Opps! forgot some files

---
+++ build.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+fn main() {
+    let slas_env_vars = [("USE_BLAS_IF_LEN_GE", "750")];
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("config.rs");
+    let mut f = File::create(&dest_path).unwrap();
+
+    for (var, default_value) in slas_env_vars {
+        let value = env::var(&format!("SLAS_{}", var)).unwrap_or(default_value.to_string());
+        f.write_all(
+            format!(
+                "
+                /// value of environments variable `SLAS_{var}` during build.
+                /// Default value is `{default_value:?}`.
+                pub const {var}: usize = {value};
+             "
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+    }
+}
